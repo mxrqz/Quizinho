217830c8348305363947446c1e1461e2f5ac0987
mxrqz <msw940220@gmail.com>
2024-09-30 18:46:14 -0300

    urlState

--- src/components/ui/stars-background.tsx
+++ src/components/ui/stars-background.tsx
@@ -88,6 +88,7 @@ export const StarsBackground: React.FC<StarBackgroundProps> = ({
 
         return () => {
             if (canvasRef.current) {
+                // eslint-disable-next-line react-hooks/exhaustive-deps
                 resizeObserver.unobserve(canvasRef.current);
             }
         };
