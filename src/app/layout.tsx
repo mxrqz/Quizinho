bea530023a8164a12b1cfb81a939b0837479643c
mxrqz <msw940220@gmail.com>
2024-09-30 21:48:24 -0300

    ads.txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Script from "next/script";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Quizinho - Crie um Quiz para Seu Amor 💖",
@@ -14,13 +15,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script
-                async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7551677366710429"
-                crossOrigin="anonymous"></Script>
-      <body
-        className={`antialiased`}
-      >
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-7551677366710429" />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7551677366710429"
+          crossOrigin="anonymous"></Script>
+      </head>
+      <body className={`antialiased`}>
         {children}
       </body>
     </html>
